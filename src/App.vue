87b693fd506f9fe3102744cc19d804c166918a10
pemeraldy <pemeraldy@gmail.com>
2020-12-09 16:48:51 +0100

    Navigation Bar changed with theme modes added

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,36 @@
 <template>
   <v-app dark>
-    <v-app-bar dark height="80" app flat>
-      <v-tabs class="ml-n9 px-4">
-        <v-tab to="/home">Mo-v-share</v-tab>
+    <v-app-bar dark app flat>
+      <v-tabs class="ml-n9 px-4" color="red darken-1">
+        <v-tab class="app-name" to="/home">Mo-v-share</v-tab>
         <v-spacer></v-spacer>
         <v-switch
           v-model="$vuetify.theme.dark"
           inset
           class="mt-4"
           label="Dark Mode"
-          persistent-hint
         ></v-switch>
         <v-spacer></v-spacer>
-        <search-form />
-        <p v-if="user" class="font-weight-regular pt-4 mr-3">
+        <!-- <search-form /> -->
+        <v-sheet v-if="user" class="font-weight-regular pt-4 mr-3">
           {{ user.username.charAt(0).toUpperCase() + user.username.slice(1) }}
-        </p>
-
-        <v-btn v-if="isLoggedIn" color="red accent-2" @click="signOut"
-          >Logout</v-btn
-        >
-        <v-sheet v-else>
-          <v-tab class="mr-2" color="red accent-2" to="/sign-up">Sign Up</v-tab>
-          <v-tab color="red accent-2" to="/login">Log In</v-tab>
         </v-sheet>
+
+        <v-tab v-if="isLoggedIn" color="red accent-2" @click="signOut"
+          >Logout</v-tab
+        >
+
+        <v-tab v-if="!isLoggedIn" color="red accent-2" to="/sign-up"
+          >Sign Up</v-tab
+        >
+        <v-tab v-if="!isLoggedIn" color="red accent-2" to="/login"
+          >Log In</v-tab
+        >
       </v-tabs>
       <v-avatar color="white mr-2" size="32"></v-avatar>
     </v-app-bar>
     <v-main>
-      <v-container grey lighten-5 fluid>
+      <v-container fluid>
         <v-fade-transition>
           <router-view />
         </v-fade-transition>
@@ -38,11 +40,11 @@
 </template>
 
 <script>
-import SearchForm from "@/components/SearchForm.vue";
+// import SearchForm from "@/components/SearchForm.vue";
 export default {
   name: "App",
   components: {
-    SearchForm,
+    // SearchForm,
   },
   computed: {
     isLoggedIn() {
@@ -69,5 +71,8 @@ export default {
 <style lang="css">
 .home-bar {
   cursor: pointer;
+}
+.app-name {
+  font-family: greatvibes;
 }
 </style>
