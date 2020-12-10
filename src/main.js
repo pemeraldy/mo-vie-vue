5a67f4a83ff174fce29381684ed3ded022f647f3
pemeraldy <pemeraldy@gmail.com>
2020-12-10 03:52:43 +0100

    set moviecollectio to empty on logout

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store/index'
 import firebaseServices from './firebase'
 Vue.config.productionTip = false
 
-
+Vue.prototype.reloadAccess = window.location
 let app
 firebaseServices.auth.onAuthStateChanged((user)=>{
   if(!app){
